9a7aae3042d5cd36b5fe1ba58e91fa62b8dcf602
Jay Lowe <jalowe@csumb.edu>
2017-09-02 18:42:10 +0000

    test

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+        <meta charset="utf-8"/>
+    
+        <style type="text/css">
+            
+            li{
+                
+            }
+            
+            h1{
+                
+                background-color: green;
+            }
+            
+        </style>
+    
+    </head>
+    <body>
+        
+    <nav>    
+        
+        <ul>
+            <li><a href="index.html"> Home </a> </li>
+            <li><a href="index.html"> About </a> </li>
+            <li><a href="index.html"> Contact Me! </a> </li>
+        </ul>
+    
+    </nav>
+    
+    <figure>
+        
+        <img src="img/x.jpg" width = "300" alt="pic of x" />
+        
+    </figure>
+    
+    <div>
+        
+        Welcome!
+        
+    </div>
+    
+
+    </body>
+</html>
