7fa5370e00c81269f8bff21095eb3df7f263bbe2
Jay Lowe <jalowe@csumb.edu>
2017-10-16 02:26:54 +0000

    lab 5

--- labs/lab5/index.php
+++ labs/lab5/index.php
@@ -1,6 +1,5 @@
 <?php
-
-include '../../../dbConnection.php';
+include '../../dbConnection.php';
 
 $conn = getDatabaseConnection();
 
@@ -22,24 +21,74 @@ function getDeviceTypes() {
 }
 
 
-function getAvailable() {
+function displayDevices(){
     global $conn;
-    $sql = "SELECT *
-            FROM `tc_device` 
-            WHERE "";
+    
+    $sql = "SELECT * FROM tc_device WHERE 1 ";
+    
+    
+    if (isset($_GET['submit'])){
+        
+        $namedParameters = array();
+        
+        
+        if (!empty($_GET['deviceName'])) {
+            
+            //The following query allows SQL injection due to the single quotes
+            //$sql .= " AND deviceName LIKE '%" . $_GET['deviceName'] . "%'";
+  
+            $sql .= " AND deviceName LIKE :deviceName"; //using named parameters
+            $namedParameters[':deviceName'] = "%" . $_GET['deviceName'] . "%";
+
+         }
+         
+        if (!empty($_GET['deviceType']) && $_GET['deviceType'] != "Select One") {
+            
+            //The following query allows SQL injection due to the single quotes
+            //$sql .= " AND deviceName LIKE '%" . $_GET['deviceName'] . "%'";
+  
+            $sql .= " AND deviceType = :dType"; //using named parameters
+            $namedParameters[':dType'] =   $_GET['deviceType'] ;
+
+         }     
+         
+         if (isset($_GET['available'])) {
+             $sql .= " AND status = 'A'"; //using named parameters
+         }
+         
+         
+         if (isset($_GET['orderBy'])) {
+             
+             if($_GET['orderBy'] == "name") {
+                 $sql .= " ORDER BY deviceName ASC";
+             }
+             else {
+                 $sql .= " ORDER BY price ASC";
+             }
+         }
+         
+        
+        
+        
+    }//endIf (isset)
+    
+    //If user types a deviceName
+     //   "AND deviceName LIKE '%$_GET['deviceName']%'";
+    //if user selects device type
+      //  "AND deviceType = '$_GET['deviceType']";
     
     $stmt = $conn->prepare($sql);
-    $stmt->execute();
+    $stmt->execute($namedParameters);
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
-    foreach ($records as $record) {
+     foreach ($records as $record) {
         
-        echo "<option> "  . $record['deviceType'] . "</option>";
+        echo  $record['deviceName'] . " " . $record['deviceType'] . " " .
+              $record['price'] .  "  " . $record['status'] . 
+              "<a target='checkoutHistory' href='checkoutHistory.php?deviceId=".$record['deviceId']."'> Checkout History </a> <br />";
         
     }
 }
-
-
 
 ?>
 
@@ -47,6 +96,7 @@ function getAvailable() {
 <html>
     <head>
         <title>Lab 5: Device Search </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         
@@ -63,9 +113,29 @@ function getAvailable() {
             <input type="checkbox" name="available" id="available">
             <label for="available"> Available </label>
             
-            <input type="submit" value="Search!" >
+            <br>
+            Order by:
+            <input type="radio" name="orderBy" id="orderByName" value="name"/> 
+             <label for="orderByName"> Name </label>
+            <input type="radio" name="orderBy" id="orderByPrice" value="price"/> 
+             <label for="orderByPrice"> Price </label>
+            
+            
+            
+            <input type="submit" value="Search!" name="submit" >
         </form>
         
+        
+        <hr>
+        
+        <?=displayDevices()?>
+        
+        
+        
+        <iframe name="checkoutHistory" width="400" height="400"></iframe>
+        
+
+
 
     </body>
 </html>
